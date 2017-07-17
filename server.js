aa420cc9bc215eeeda930905fdf48c654503ea59
Chris Dianne Calamba <Chris Dianne Calamba>
2017-07-17 17:37:48 +0800

    First Commit

---
+++ server.js
@@ -0,0 +1,31 @@
+//server.js
+//require modules
+var express = require('express');
+var path = require('path');
+//instantiate express
+var app = express();
+//set port
+app.set('port', (process.env.PORT || 5000));
+//use static files
+app.use(express.static(path.join(__dirname, 'public/')));
+//express routes
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, 'view/FORM.html'));
+});
+app.get('/FORM.html', function(req, res){
+  res.sendFile(path.join(__dirname, 'view/FORM.html'));
+});
+app.get('/logged.html', function(req, res){
+  res.sendFile(path.join(__dirname, 'view/logged.html'));
+});
+app.get('/register.html', function(req, res){
+  res.sendFile(path.join(__dirname, 'view/register.html'));
+});
+
+
+
+
+//express server listen
+var server = app.listen(app.get('port'), function(){
+  console.log('Server listening on port ',app.get('port'));
+});
